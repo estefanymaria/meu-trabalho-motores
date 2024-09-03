b8e0675a85e496dc47dd61a1da5f9304c36ca4a9
estefanymaria <estefanymaria0908@gmail.com>
2024-09-03 10:15:20 -0300

    Moeda

---
+++ Meu Game 3D/Assets/Scripts/Moeda.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moeda : MonoBehaviour
+{
+    public int velocidadeGiro = 5;
+    
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player");
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * velocidadeGiro * Time.deltaTime);
+    }
+}
